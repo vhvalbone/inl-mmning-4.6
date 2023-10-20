55439728c09e62b1932d16b44c4024515d98e23f
alsv06003 <alsv06003@E-PW0234JW>
2023-10-20 12:55:04 +0200

    Add project files.

---
+++ inlämmning 4.6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace inlämmning_4._6
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett meddelande");
+            string meddelande = Console.ReadLine();
+
+            Console.WriteLine("Ange antal steg åt höger");
+            int steg = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                for (int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(meddelande[i]);
+            }
+        }
+    }
+}
